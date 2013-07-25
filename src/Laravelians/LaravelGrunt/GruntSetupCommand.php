5d846d72ffa5add3f2fbf8f7f964e69cef9d5f2e
JasonMortonNZ <jasonlmorton@gmail.com>
2013-07-26 11:33:10 +1200

    Initial commit

---
+++ src/Laravelians/LaravelGrunt/GruntSetupCommand.php
@@ -0,0 +1,201 @@
+<?php namespace Laravelians\LaravelGrunt;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Config\Repository as Config;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class GruntSetupCommand extends Command {
+
+	/**
+	 * The console command name
+	 * 
+	 * @var string
+	 */
+	protected $name = 'grunt:setup';
+
+	/**
+	 * The consolse command description
+	 * 
+	 * @var string
+	 */
+	protected $description = 'Generate a new Gruntfile.js';
+
+	/**
+	 * Path to the assets folder
+	 * 
+	 * @var string
+	 */
+	protected $assetsPath;
+
+	/**
+	 * List of user required grunt plugins
+	 * 
+	 * @var array
+	 */
+	protected $plugins = array();
+
+	/**
+	 * GruntGenerator Instance
+	 * 
+	 * @var GruntGenerator
+	 */
+	protected $generator;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param GruntGenerator $generator
+	 * @param Config         $config
+	 */
+	public function __construct(GruntGenerator $generator, Config $config)
+	{
+		parent::__construct();
+
+		$this->generator  = $generator;
+		$this->assetsPath = $config->get('laravel-grunt::assets_path');
+	}
+
+	/**
+	 * Execute the console command
+	 * 
+	 * @return void
+	 */
+	public function fire()
+	{
+		// If user has both node and npm installed continue
+		if($this->hasNode() && $this->hasNpm())
+		{
+			$this->info('Node and NPM install. Continue...');
+			$this->askQuestions();
+
+			// Create assets folder
+			$this->generator->createAssetsFolder();
+			$this->info('Assets folder (public/assets/) - successfully created!');
+
+			// Create package.json file
+			$this->generator->createPackageFile();
+			$this->info('package.json successfully created!');
+
+			// Generate custom gruntfile.js
+			$this->generator->createGruntFile($this->plugins);
+			$this->info('Gruntfile.js successfully created!');
+
+			// Add node_modules folder to .gitignore
+			$this->generator->addToGitingnore('.gitignore', "\n/node_modules");
+
+			// Install / update modules
+			$this->info('Installing / updating required grunt plugins...');
+			shell_exec('sudo npm install');
+		}
+		else
+		{
+			$this->error('It appears that either node or npm is not installed. Please install and try again!');
+			exit();
+		}
+	}
+
+	/**
+	 * Ask the user which plugins they require
+	 * 
+	 * @return void
+	 */
+	protected function askQuestions()
+	{
+		// Do they want preprocessing?
+		$this->wantPreprocessing();
+		// Do they want phpUnit testing?
+		$this->wantPhpUnitTesting();
+		// Do they want js linting?
+		$this->wantJsLinting();
+		// Do they require live reloading?
+		$this->wantLiveReloading();
+	}
+
+	/**
+	 * Ask user which preprocessor they require
+	 * 
+	 * @return void
+	 */
+	protected function wantPreprocessing()
+	{
+		if($this->confirm('Do you require CSS preprocessing? [yes|no]', false))
+		{
+			// Get answer from user
+			$preprocessor = strtolower($this->ask('Which CSS preprocessor do you require? [less|sass|stylus]'));
+
+			// While answer is not valid, ask again
+			while( ! $preprocessor || ! in_array($preprocessor, array('less', 'sass', 'stylus')))
+			{
+				$preprocessor = $this->ask('I did not recognize that preprocessor. Please try again. [less|sass|stylus]');
+			}
+
+			$this->plugins[] = $preprocessor;
+		}
+	}
+
+	/**
+	 * Ask user is they require phpunit testing
+	 * 
+	 * @return void
+	 */
+	protected function wantPhpUnitTesting()
+	{
+		if($this->confirm('Do you require PHPUnit testing? [yes|no]', false))
+		{
+			$this->plugins[] = 'phpunit';
+		}
+	}
+
+	/**
+	 * Ask user is they require javascript linting
+	 * 
+	 * @return void
+	 */
+	protected function wantJsLinting()
+	{
+		if($this->confirm('Do you require Javascript linting (JSHint)? [yes|no]', false))
+		{
+			$this->plugins[] = 'jshint';
+		}
+	}
+
+	/**
+	 * Ask user is they require live reloading
+	 * 
+	 * @return void
+	 */
+	protected function wantLiveReloading()
+	{
+		if($this->confirm('Do you live browser reloading? [yes|no]', false))
+		{
+			$this->plugins[] = 'reload';
+		}
+	}
+
+	/**
+	 * Check if user has node installed
+	 * 
+	 * @return boolean
+	 */
+	protected function hasNode()
+	{
+		$node = shell_exec('node -v');
+
+		return starts_with($node, 'v');
+	}
+
+	/**
+	 * Check if user has npm (node package manager) installed
+	 * 
+	 * @return boolean
+	 */
+	protected function hasNpm()
+	{
+		$npm = shell_exec('npm -v');
+
+		return starts_with($npm, '1.');
+	}
+	
+}
---
+++ src/config/config.php
@@ -0,0 +1,137 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Assets Folder Base Path
+	|--------------------------------------------------------------------------
+	|
+	| This is where you can specify a custom path to your assets
+	| directory. We've set a sensible default, but feel free to update it.
+	|
+	*/
+	'assets_path' => 'public/assets',
+
+	/*
+	|--------------------------------------------------------------------------
+	| The CSS Path
+	|--------------------------------------------------------------------------
+	|
+	| This is where you can specify a custom path to your CSS
+	| directory. We've set a sensible default, but feel free to update it.
+	|
+	*/
+	'css_path' => '/css',
+
+	/*
+	|--------------------------------------------------------------------------
+	| The CSS File Order
+	|--------------------------------------------------------------------------
+	|
+	| This is where you can specify a custom order in which
+	| CSS files will be concatenated, compiled and minified.
+	| We've set a sensible default, but feel free to update it.
+	|
+	| E.g:
+	| 'css_files' => '["style1.css", "style2.css", "style3.css"]',
+	|
+	*/
+	'css_files' => '["*.css"]',
+
+	/*
+	|--------------------------------------------------------------------------
+	| The JavaScript Path
+	|--------------------------------------------------------------------------
+	|
+	| This is where you can specify a custom path to your JavaScript
+	| directory. We've set a sensible default, but feel free to update it.
+	|
+	*/
+	'js_path' => '/js',
+
+	/*
+	|--------------------------------------------------------------------------
+	| The JavaScript File Order
+	|--------------------------------------------------------------------------
+	|
+	| This is where you can specify a custom order in which
+	| JavaScript files will be concatenated, compiled and minified.
+	| We've set a sensible default, but feel free to update it.
+	|
+	| E.g:
+	| 'js_files' => '["script1.js", "script2.js", "script3.js"]',
+	|
+	*/
+	'js_files' => '["*.js"]',
+
+	/*
+	|--------------------------------------------------------------------------
+	| The LESS Path
+	|--------------------------------------------------------------------------
+	|
+	| This is where you can specify a custom path to your LESS
+	| directory. We've set a sensible default, but feel free to update it.
+	|
+	*/
+	'less_path' => '/less',
+
+	/*
+	|--------------------------------------------------------------------------
+	| The Main LESS file
+	|--------------------------------------------------------------------------
+	|
+	| This is where you can specify a custom path to your main LESS
+	| file, which should include all imports to other LESS files.
+	| We've set a sensible default, but feel free to update it.
+	|
+	*/
+	'less_file' => 'main.less',
+
+	/*
+	|--------------------------------------------------------------------------
+	| The SASS Path
+	|--------------------------------------------------------------------------
+	|
+	| This is where you can specify a custom path to your SASS
+	| directory. We've set a sensible default, but feel free to update it.
+	|
+	*/
+	'sass_path' => '/sass',
+
+	/*
+	|--------------------------------------------------------------------------
+	| The Main SASS file
+	|--------------------------------------------------------------------------
+	|
+	| This is where you can specify a custom path to your main SASS
+	| file, which should include all imports to other SASS files.
+	| We've set a sensible default, but feel free to update it.
+	|
+	*/
+	'sass_file' => 'main.sass',
+
+	/*
+	|--------------------------------------------------------------------------
+	| The Stylus Path
+	|--------------------------------------------------------------------------
+	|
+	| This is where you can specify a custom path to your Stylus
+	| directory. We've set a sensible default, but feel free to update it.
+	|
+	*/
+	'stylus_path' => '/stylus',
+
+	/*
+	|--------------------------------------------------------------------------
+	| The Main Stylus file
+	|--------------------------------------------------------------------------
+	|
+	| This is where you can specify a custom path to your main Stylus
+	| file, which should include all imports to other Stylus files.
+	| We've set a sensible default, but feel free to update it.
+	|
+	*/
+	'stylus_file' => 'main.stylus',
+
+);
